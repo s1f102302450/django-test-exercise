cffeceea9a1c66c0d63e17f75a6ad78c45683e1c
1F10230245 <s1f102302450@iniad.org>
2024-06-27 13:30:57 +0900

    Initial Commit

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = ''ja
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
